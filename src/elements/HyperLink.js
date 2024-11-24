713e6f0ad5725909d17e2e7232d5d5c83788c2aa
circleu <shawndev25@gmail.com>
2024-11-25 05:35:46 +0900

    Update

--- src/elements/HyperLink.js
+++ src/elements/HyperLink.js
@@ -1,16 +1,11 @@
 import React from "react";
 
-
-function Goto(link) {
-  window.location.href = link;
-}
-
 function HyperLink({img, label, text, link}) {
   if (img == null) {
     return (
       <div className="hyperlink-body">
         <span className="hyperlink-label">{label}{":"}&nbsp;</span>
-        <span className="hyperlink-link" onClick={() => {Goto(link)}}>{text}</span>
+        <a className="hyperlink-link" href={link}>{text}</a>
       </div>
     );
   }
@@ -19,7 +14,7 @@ function HyperLink({img, label, text, link}) {
       <div className="hyperlink-body">
         <img className="hyperlink-img" src={img}></img>
         <span className="hyperlink-label">{label}{":"}&nbsp;</span>
-        <span className="hyperlink-link" onClick={() => {Goto(link)}}>{text}</span>
+        <a className="hyperlink-link" href={link}>{text}</a>
       </div>
     );
   }
